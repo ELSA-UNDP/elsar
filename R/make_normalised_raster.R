89677bf633f8f112bb33b2e0d28ecb7910613873
Scott Consaul Atkinson <scottconsaulatkinson@gmail.com>
2024-10-29 16:35:35 +1100

    Add datatype check and writeRaster formats

--- R/make_normalised_raster.R
+++ R/make_normalised_raster.R
@@ -1,36 +1,3 @@
-#' Function to extract general raster data
-#'
-#' `make_normalised_raster()`allows you to align, normalise and save a raster file. This is applicable for the following data
-#' This is applicable for the following data sets: certified forests, drought risk, flood risk, intact wilderness area, soc difference, threatened species richness, voc, wad convergence evidence
-#'
-#' @param raster_in A `SpatRaster` file that contains the data to be put into right format
-#' @param pus A `SpatRaster` file that contains the reference spatial extent, crs etc.in form of the planning units
-#' @param iso3 A string of the iso3 name of the data (country name)
-#' @param invert Logical. Default is FALSE. If TRUE, highest values in the original dataset should be valued the lowest in the prioritisation.
-#' @param rescaled Logical. Default is TRUE. Should remain TRUE unless there is a very specific reason not to rescale.
-#' @param name_out A string with the data name that will be used for the output `tif`file
-#' @param output_path An optional output path for the created file.
-#'
-#' @return A `SpatRaster` file that has been aligned and normalised
-#' @export
-#'
-#' @examples
-#' boundary_proj <- make_boundary(
-#'   boundary_in = boundary_dat,
-#'   iso3 = "NPL",
-#'   iso3_column = "iso3cd",
-#'   do_project = TRUE
-#' )
-#'
-#' pus <- make_planning_units(boundary_proj = boundary_proj,
-#'                            pu_size = NULL,
-#'                            pu_threshold = 8.5e5,
-#'                            limit_to_mainland = FALSE)
-#' wad_dat <- get_wad_data()
-#'
-#' wadOut <- make_normalised_raster(raster_in = wad_dat,
-#'                                  pus = pus,
-#'                                  iso3 = "NPL")
 make_normalised_raster <- function(raster_in,
                                    pus,
                                    iso3,
@@ -38,37 +5,53 @@ make_normalised_raster <- function(raster_in,
                                    rescaled = TRUE,
                                    name_out,
                                    output_path = NULL) {
-  # reprojecting the global data would take too long
-  # to speed up: reproject PUs to projection of global data first
+  # Reproject the planning units (PUs) to match the projection of the input raster
   pus_reproject <- terra::project(pus, terra::crs(raster_in))
 
-  # then crop (make data a lot smaller)
+  # Crop the input raster to the extent of the planning units
   dat_aligned <- terra::crop(raster_in, pus_reproject)
 
+  # Replace NA values in the cropped raster with 0
   dat_aligned[is.na(dat_aligned)] <- 0
 
+  # Align projection and extent of the raster with the planning units
   dat_aligned <- dat_aligned %>%
-    terra::project(., terra::crs(pus)) %>% # reproject the data to the crs we actually want (the original pu crs)
-    terra::resample(., pus) %>%
-    terra::mask(pus, maskvalues = 0) # maskvalues denotes the background value in the raster that's not data (since this should always be planning region/units is 1 and outside is 0, this is hard-coded to 0)
+    terra::project(terra::crs(pus)) %>%  # Project to original PU CRS
+    terra::resample(pus) %>%             # Resample to match resolution and extent of PUs
+    terra::mask(pus, maskvalues = 0)     # Mask areas outside of planning units (assuming outside = 0)
 
+  # Invert values if required
   if (invert) {
-    dat_aligned = -dat_aligned
+    dat_aligned <- -dat_aligned
   }
 
+  # Rescale the raster if needed
   if (rescaled) {
     dat_aligned <- rescale_raster(dat_aligned)
   } else {
     warning("NOTE: Raster is NOT rescaled.")
   }
 
+  # Save the output file if output_path is provided
   if (!is.null(output_path)) {
-    terra::writeRaster(dat_aligned,
-                       glue::glue("{output_path}/{name_out}_{iso3}.tif"),
-                       gdal = c("COMPRESS=DEFLATE"),
-                       NAflag = -9999,
-                       overwrite = TRUE,
-                       filetype = "COG"
+    # Determine the appropriate data type and NoData value for saving the raster
+    if (terra::is.int(dat_aligned)) {
+      data_type <- "INT2S"  # 16-bit signed integer
+      na_value <- 255       # Byte-specific NoData value
+    } else {
+      data_type <- "FLT4S"  # 32-bit float
+      na_value <- -9999     # Float-specific NoData value
+    }
+
+    # Write the raster with the determined settings
+    terra::writeRaster(
+      dat_aligned,
+      glue::glue("{output_path}/{name_out}_{iso3}.tif"),
+      gdal = c("COMPRESS=ZSTD", "NUM_THREADS=4", "OVERVIEWS=NONE", "PREDICTOR=3"),
+      datatype = data_type,
+      NAflag = na_value,
+      overwrite = TRUE,
+      filetype = "COG"
     )
   }
 
