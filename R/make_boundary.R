d07bf087b4975cbf267a447eb4e1ab5b09b04c77
Scott Consaul Atkinson <scottconsaulatkinson@gmail.com>
2024-11-05 18:55:35 +1100

    Update docs

--- R/make_boundary.R
+++ R/make_boundary.R
@@ -24,9 +24,9 @@ make_boundary <- function(boundary_in,
                           limit_to_mainland = FALSE,
                           col_name = NULL,
                           filter_out = 0,
-                          do_project = FALSE,
+                          custom_projection = TRUE,
                           iso3 = NULL,
-                          iso3_column,
+                          iso3_column = NULL,
                           output_path = NULL) {
   #load the file with load_file() outside of this function and then use it as input here
   if (input_type == "sf") {
@@ -40,8 +40,7 @@ make_boundary <- function(boundary_in,
   if (limit_to_mainland == TRUE) { # exclude any islands etc.
     nb <- nb %>%
       sf::st_cast("POLYGON") %>%
-      dplyr::slice(which.max(as.numeric(sf::st_area(.data)))) %>% # get largest polygon that represents mainland
-      sf::st_transform(nb, crs = sf::st_crs(4326))
+      dplyr::slice(which.max(as.numeric(sf::st_area(.data)))) # get largest polygon that represents mainland
   } else {
     if (!is.null(col_name)) {
       nb <- nb %>%
@@ -56,8 +55,8 @@ make_boundary <- function(boundary_in,
       dplyr::filter(!!rlang::sym(iso3_column) == iso3)
   }
 
-  if (do_project) {
-    wkt <- make_custom_projection(boundary = nb, output_path = output_path, iso3 = iso3)
+  if (custom_projection) {
+    wkt <- make_custom_projection(boundary = nb, output_path = output_path, iso3_column = iso3_column, iso3 = iso3)
     nb <- sf::st_transform(nb, crs = sf::st_crs(wkt))
   }
 
