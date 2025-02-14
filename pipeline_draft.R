536cdedb87304f5e31db94b63d1b397d09b10832
Sandra Neubert <77614048+sandra-neubert@users.noreply.github.com>
2025-02-14 16:38:10 +1000

    update pipeline_draft

--- pipeline_draft.R
+++ pipeline_draft.R
@@ -1,7 +1,6 @@
-
 library(devtools)
 load_all()
-#library(elsar)
+# library(elsar)
 library(sf)
 library(tidyverse)
 library(exactextractr)
@@ -27,41 +26,43 @@ iso3 <- "NPL"
 iso3_column <- "iso3cd"
 
 # Set actions to do
-#create_boundary <- 1 #now in csv
-#create_PUs <- 1 #now in csv
-#create_featureStack <- 1 #now in csv
+# create_boundary <- 1 #now in csv
+# create_PUs <- 1 #now in csv
+# create_featureStack <- 1 #now in csv
 create_zones <- 0
 create_lockedIn <- 0
 
 #################################################################################
 # Load data file
 data_info <- read_delim(file.path(sheet_path, "input_data.csv"),
-                        delim = ";", escape_double = FALSE,
-                        trim_ws = TRUE) %>%
+  delim = ";", escape_double = FALSE,
+  trim_ws = TRUE
+) %>%
   dplyr::filter(include == 1) %>%
-  dplyr::mutate(full_name =
-  dplyr::case_when(
-    file_type != "postgres" ~ paste0(file_name, ".", file_type),
-    file_type == "postgres" ~ file_name,
-    file_type == NA  ~ file_name
-  ),
-  full_path =
-    dplyr::case_when(
-      (file_path == "default" | is.na(file_path)) ~ input_path,
-      file_type == "postgres" ~ NA,
-      (!is.na(file_path) & file_path != "default" & file_type != "postgres")  ~ file_path
-    )
-    )
+  dplyr::mutate(
+    full_name =
+      dplyr::case_when(
+        file_type != "postgres" ~ paste0(file_name, ".", file_type),
+        file_type == "postgres" ~ file_name,
+        file_type == NA ~ file_name
+      ),
+    full_path =
+      dplyr::case_when(
+        (file_path == "default" | is.na(file_path)) ~ input_path,
+        file_type == "postgres" ~ NA,
+        (!is.na(file_path) & file_path != "default" & file_type != "postgres") ~ file_path
+      )
+  )
 
 # Postgres check
 if (nrow(data_info %>%
-         dplyr::filter(file_type == "postgres")) > 0) {
+  dplyr::filter(file_type == "postgres")) > 0) {
   {
-    host <- readline(prompt = "Enter host: ");
-    dbname <- readline(prompt = "Enter database name: ");
-    port <- readline(prompt = "Enter port: ");
-    user <- readline(prompt = "Enter user name: ");
-    password <- readline(prompt = "Enter password: ");
+    host <- readline(prompt = "Enter host: ")
+    dbname <- readline(prompt = "Enter database name: ")
+    port <- readline(prompt = "Enter port: ")
+    user <- readline(prompt = "Enter user name: ")
+    password <- readline(prompt = "Enter password: ")
   }
 
   postgres_dict <- elsar::make_postgres_connection(
@@ -78,9 +79,8 @@ if (nrow(data_info %>%
 # Create data
 ## PUs
 if (data_info %>%
-    dplyr::filter(group == "pus") %>%
-    dplyr::pull(include)) {
-
+  dplyr::filter(group == "pus") %>%
+  dplyr::pull(include)) {
   message("Creating Planning Units")
 
   ### Load data
@@ -88,18 +88,18 @@ if (data_info %>%
     dplyr::filter(group == "boundary")
 
   boundary_dat <- elsar::elsar_load_data(
-      file_name = boundary_info$file_name,
-      file_path = (if (boundary_info$file_path == "NULL") NULL else boundary_info$file_path), #need this otherwise NULL is read as character
-      file_type = boundary_info$file_type,
-      db_info = postgres_dict,
-      iso3 = iso3,
-      iso3_column = iso3_column
-    )
+    file_name = boundary_info$file_name,
+    file_path = (if (boundary_info$file_path == "NULL") NULL else boundary_info$file_path), # need this otherwise NULL is read as character
+    file_type = boundary_info$file_type,
+    db_info = postgres_dict,
+    iso3 = iso3,
+    iso3_column = iso3_column
+  )
 
   ### Create boundary
   if (as.logical(data_info %>%
-      dplyr::filter(group == "boundary") %>%
-      dplyr::pull(include))) {
+    dplyr::filter(group == "boundary") %>%
+    dplyr::pull(include))) {
     boundary_proj <- make_boundary(
       boundary_in = boundary_dat,
       iso3 = iso3,
@@ -112,19 +112,21 @@ if (data_info %>%
   ### Create PUs
   defaults_pu <- data_info %>%
     dplyr::filter(group == "pus") %>%
-    dplyr::pull(default_parameters) #get whether to use default values or not
+    dplyr::pull(default_parameters) # get whether to use default values or not
 
-  if (defaults_pu) { #use default values
-    pus <- elsar::make_planning_units(boundary_proj = boundary_proj,
-                               pu_size = NULL,
-                               pu_threshold = 8.5e5,
-                               limit_to_mainland = FALSE)
-  } else if (!defaults_pu ) { # ask user to supply inputs manually (either in pop up or console)
+  if (defaults_pu) { # use default values
+    pus <- elsar::make_planning_units(
+      boundary_proj = boundary_proj,
+      pu_size = NULL,
+      pu_threshold = 8.5e5,
+      limit_to_mainland = FALSE
+    )
+  } else if (!defaults_pu) { # ask user to supply inputs manually (either in pop up or console)
 
     {
-      pu_size <- readline(prompt = "Enter a custom planning unit size; can be NULL if maximum number of PUs is provided: ");
-      pu_threshold <- readline(prompt = "Enter a maximum PU number; affects all subsequent run time: ");
-      limit_to_mainland <- readline(prompt = "Enter whether to include only mainland; TRUE or FALSE: ");
+      pu_size <- readline(prompt = "Enter a custom planning unit size; can be NULL if maximum number of PUs is provided: ")
+      pu_threshold <- readline(prompt = "Enter a maximum PU number; affects all subsequent run time: ")
+      limit_to_mainland <- readline(prompt = "Enter whether to include only mainland; TRUE or FALSE: ")
     }
 
     if (pu_size == "NULL") {
@@ -136,37 +138,38 @@ if (data_info %>%
     pu_threshold <- as.integer(pu_threshold)
     limit_to_mainland <- as.logical(limit_to_mainland)
 
-    pus <- make_planning_units(boundary_proj = boundary_proj,
-                               pu_size = pu_size,
-                               pu_threshold = pu_threshold,
-                               limit_to_mainland = limit_to_mainland)
+    pus <- make_planning_units(
+      boundary_proj = boundary_proj,
+      pu_size = pu_size,
+      pu_threshold = pu_threshold,
+      limit_to_mainland = limit_to_mainland
+    )
   } else {
     message("Default input needs to be 0 (use custom settings) or 1 (default values)")
   }
 
   ### Visualise PUs
   if (as.logical(data_info %>%
-      dplyr::filter(group == "pus") %>%
-      dplyr::pull(visualise))) {
+    dplyr::filter(group == "pus") %>%
+    dplyr::pull(visualise))) {
     print("TBA")
   }
-
 }
 
-## Create feature stack
+## Create feature stack #could have one master script and then extra feature generation scripts etc that are sourced + ADD visualisation option
 # Assign data to functions
-feature_list <- data_info %>% #get all included features
+feature_list <- data_info %>% # get all included features
   dplyr::filter(
     group == "feature",
     include == 1
   )
 
-dat_default <- feature_list %>% #get those feature names that run with make_normalised_raster()
+dat_default <- feature_list %>% # get those feature names that run with make_normalised_raster()
   dplyr::filter(default_function == 1) %>%
   dplyr::select("data_name") %>%
   dplyr::pull()
 
-dat_non_default <- feature_list %>% #get those features that have their own function
+dat_non_default <- feature_list %>% # get those features that have their own function
   dplyr::filter(default_function == 0) %>%
   dplyr::select("data_name") %>%
   dplyr::pull()
@@ -176,7 +179,7 @@ cat("Creating Default Features")
 
 # init data stack with pus as first layer
 raster_out <- pus
-for (i in 1:length(dat_default)) { #for all the data that runs with make_normalised_raster()
+for (i in 1:length(dat_default)) { # for all the data that runs with make_normalised_raster()
   current_dat <- feature_list %>%
     dplyr::filter(data_name == dat_default[[i]])
 
@@ -188,11 +191,11 @@ for (i in 1:length(dat_default)) { #for all the data that runs with make_normali
     file_type = current_dat$file_type, file_path = current_dat$full_path
   )
 
-  if (current_dat$default_parameters != 1) { #if non default parameters in make_normalised_raster()
+  if (current_dat$default_parameters != 1) { # if non default parameters in make_normalised_raster()
     answer <- readline("Non-default option selected. Do you want to use pre-saved options? (yes/no): ")
-    answer <- tolower(trimws(answer)) #to streamline if someone says "Yes" or similar
+    answer <- tolower(trimws(answer)) # to streamline if someone says "Yes" or similar
 
-    if (answer == "yes") { #go through pre-saved options that we need to list down here. Will add to this
+    if (answer == "yes") { # go through pre-saved options that we need to list down here. Will add to this
       if (current_dat$data_name == "Crop Suitability Difference") {
         rast_norm <- make_normalised_raster(
           raster_in = current_rast,
@@ -206,16 +209,16 @@ for (i in 1:length(dat_default)) { #for all the data that runs with make_normali
         cat("TBA")
       } else {
         cat("Your data might not have a pre-saved option yet. Please enter your processing options manually.")
-        next #jump to else if to enter options manually. Not tested yet!!!
+        next # jump to else if to enter options manually. Not tested yet!!!
       }
-    } else if (answer == "no") { #manually add processing options
+    } else if (answer == "no") { # manually add processing options
       cat("Please enter your processing options manually\n")
 
       {
-        invert <- readline(prompt = "Invert data (TRUE/FALSE; Default FALSE): ");
-        rescaled <- readline(prompt = "Rescale data (TRUE/FALSE; Default TRUE): ");
-        method_override <- readline(prompt = "Override projection method (any valid terra projection method or NULL): ");
-        conditional_expression <- readline(prompt = "Enter a conditional expression (or NULL if not needed): ");
+        invert <- readline(prompt = "Invert data (TRUE/FALSE; Default FALSE): ")
+        rescaled <- readline(prompt = "Rescale data (TRUE/FALSE; Default TRUE): ")
+        method_override <- readline(prompt = "Override projection method (any valid terra projection method or NULL): ")
+        conditional_expression <- readline(prompt = "Enter a conditional expression (or NULL if not needed): ")
       }
 
       if (method_override == "NULL") {
@@ -233,11 +236,10 @@ for (i in 1:length(dat_default)) { #for all the data that runs with make_normali
         output_path = output_path,
         name_out = dat_default[[i]]
       )
-
     } else {
       cat("Invalid input. Please enter 'yes' or 'no'.\n")
     }
-  } else { #everything default
+  } else { # everything default
     rast_norm <- make_normalised_raster(
       raster_in = current_rast,
       pus = pus,
@@ -254,12 +256,117 @@ for (i in 1:length(dat_default)) { #for all the data that runs with make_normali
 ### Prep data using non-default methods
 cat("Creating Non-Default Features")
 
+for (j in 1:length(dat_non_default)) { # for all the data that runs with non-default functions
+
+  current_dat <- feature_list %>%
+    dplyr::filter(data_name == dat_non_default[[j]])
+
+  print(dat_default[[j]])
+
+  # # load data
+  # current_rast <- elsar_load_data(
+  #   file_name = current_dat$full_name,
+  #   file_type = current_dat$file_type, file_path = current_dat$full_path
+  # )
+
+  if (j == "Mangroves") {
+    print("Mangroves")
+
+    current_rast <- elsar_load_data(
+      file_name = current_dat$full_name,
+      file_type = current_dat$file_type, file_path = current_dat$full_path,
+      wkt_filter = TRUE,
+      bb_extend = pus
+    )
+
+    mangrove_raster <- make_mangroves(
+      sf_in = current_rast,
+      pus = pus,
+      iso3 = iso3,
+      output_path = output_path,
+      name_out = dat_default[[i]]
+    )
+    raster_out <- c(raster_out, mangrove_raster)
+  }
+
+  if (j == "Forest Integrity Index") { # add saving
+
+    current_dat <- feature_list %>%
+      dplyr::filter(data_name == dat_non_default[[j]])
+
+    print("Forest Integrity Index")
+
+    if (grepl(",", current_dat$file_name)) {
+      fii_names <- unlist(strsplit(current_dat$file_name, ", ", fixed = TRUE))
+
+      file_labels <- c("flii", "fsii")
+      named_files <- setNames(
+        fii_names[sapply(
+          file_labels,
+          function(x) {
+            any(grepl(x,
+              fii_names,
+              ignore.case = TRUE
+            ))
+          }
+        )],
+        file_labels
+      )
+
+      # load data
+      raster_flii <- elsar_load_data(
+        file_name = paste0(named_files[["flii"]], ".", current_dat$file_type),
+        file_type = current_dat$file_type, file_path = current_dat$full_path
+      )
+
+      raster_fsii <- elsar_load_data(
+        file_name = paste0(named_files[["fsii"]], ".", current_dat$file_type),
+        file_type = current_dat$file_type, file_path = current_dat$full_path
+      )
+
+      # forest integrity
+      forest_integrity <- make_forest_integrity(
+        raster_flii = raster_flii,
+        raster_fsii = raster_fsii,
+        pus = pus
+      )
+    } else if ((!(grepl(",", current_dat$file_name))) & (grepl("flii", current_dat$file_name))) {
+      # load data
+      raster_flii <- elsar_load_data(
+        file_name = current_dat$full_name,
+        file_type = current_dat$file_type, file_path = current_dat$full_path
+      )
+
+      # forest integrity
+      forest_integrity <- make_forest_integrity(
+        raster_flii = raster_flii,
+        pus = pus
+      )
+    } else if ((!(grepl(",", current_dat$file_name))) & (grepl("fsii", current_dat$file_name))) {
+      # load data
+      raster_fsii <- elsar_load_data(
+        file_name = current_dat$full_name,
+        file_type = current_dat$file_type, file_path = current_dat$full_path
+      )
+
+      # forest integrity
+      forest_integrity <- make_forest_integrity(
+        raster_fsii = raster_fsii,
+        pus = pus
+      )
+    }
+    raster_out <- c(raster_out, forest_integrity)
+  }
+}
+
 
 ## Create zones
 zones_list
 zones_data <- data_info %>%
-  dplyr::filter(group == "zone",
-                include == 1)
+  dplyr::filter(
+    group == "zone",
+    include == 1
+  )
 
 ## Create locked-in areas
 lockedIn_list <- c("avail")
